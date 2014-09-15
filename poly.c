f1ed40061b95d3f3224529824b2c70e029583f9c
Keith Yong <terda12@gmail.com>
2014-09-15 15:39:12 -0400

    Finalized the program

--- poly.c
+++ poly.c
@@ -1,27 +1,49 @@
 #include <stdio.h>
 #include <string.h>
-
+#include <math.h>
+ 
 #define ARRAY_SIZE	100
-
+ 
 void shiftArrayBackByOne(int a[]){
 	for(int i = 1; i <= ARRAY_SIZE; i++){
 		a[i - 1] = a[i];
 	}
 }
 
+void printPoly(int a[], int n) {
+	for(int i = 0; i < n; i++) {
+	if(i < n - 1) {
+		printf("%dx^%d + ", a[i], i);
+	}
+	else {
+		printf("%dx^%d \n", a[i], i);
+	}
+	}
+}
+ 
 void printArray(int a[], int n){
 	for(int i = 0; i < n; i++){
 		printf("%d ", a[i]);
 	}
 	putchar('\n');
 }
-
+ 
 void fillArrayWithZeroes(int a[], int n){
 	for(int i = 0; i < n; i++){
 		a[i] = 0;
 	}
 }
 
+int fofx(int a[], int n, int x) {
+	int sum = 0;
+	for(int i = 0; i < n; i++) {
+		int value = x;
+		value = a[i] * pow(value, i);
+		sum = sum + value;
+	}
+	return sum;
+}
+ 
 int main(){
 	int isLineTwo = 0;
 	int countOne = 0;
@@ -31,30 +53,33 @@ int main(){
 	int num2;
 	int array1[ARRAY_SIZE];
 	int array2[ARRAY_SIZE];
-
+	int results[2 * ARRAY_SIZE];
+ 
 	fillArrayWithZeroes(array1, ARRAY_SIZE);
 	fillArrayWithZeroes(array2, ARRAY_SIZE);
-
+	fillArrayWithZeroes(results, (2 * ARRAY_SIZE));
+ 
 	char ch;
-
+ 
 	while(scanf("%d%c", &inputNum, &ch) > 0){
-
+ 
 		//Puts the input into different arrays depeding
 		//on value of isLineTwo
-		if (isLineTwo){
+		if (isLineTwo == 0){
 			array2[countOne] = inputNum;
 			countOne++;
-		} else {
+		} 
+		else if(isLineTwo == 1) {
 			array1[countTwo] = inputNum;
 			countTwo++;
 		}
-
+ 
 		//Increment isLineTwo if ch is a 'newline'
 		if (ch == '\n')
 		{
 			isLineTwo++;
 		}
-
+ 
 		//Check for bad stdin inputs
 		if (isLineTwo > 1){
 			printf("Hey, no more than 2 input lines!\n");
@@ -78,20 +103,31 @@ int main(){
         	printf("Please don't put more than 100 numbers in one line");
         	return -1;
         }
-
+ 
 	}
 	printArray(array1, countOne);
 	printArray(array2, countTwo);
-
+ 
 	num1 = array1[0];
 	num2 = array2[0];
-
+ 
 	shiftArrayBackByOne(array1);
 	shiftArrayBackByOne(array2);
-
+ 
 	printf("num1 = %d\n", num1);
 	printf("num2 = %d\n", num2);
-
-	printArray(array1, countOne);
-	printArray(array2, countTwo);
+ 
+	printPoly(array1, countOne - 1);
+	printPoly(array2, countTwo - 1);
+	
+	for(int i = 0; i < num2; i++) {
+		for(int j = 0; j < num1; j++) {
+			results[i + j] = results[i + j] + (array2[i] * array1[j]);
+		}
+	}
+	
+	printPoly(results, (num1 + num2 - 1));
+	printf("f(1)g(1) = %d\n", fofx(results, (num1 + num2 - 1), 1));
+	
+	
 }
